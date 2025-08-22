7e93f8879457069475f1121de525397fd86dd772
username <useremail>
2025-08-22 13:20:58 -0700

    finish week 5

--- Cis110_Pizza_ordering_chatbot.py
+++ Cis110_Pizza_ordering_chatbot.py
@@ -1,28 +1,22 @@
 print("Hello, my name is Bobby Brown your virtual assistant.I will helo you order a pizza")
 userName = input("\nEnter your name:  ")
-while len(userName) ==0:
-    userName = input("please enter your name:")
+while len {userName} == 0 :
+    userName = input("Name cannot be blank! Please enter your name:  ")
+
 if userName.lower() == "seirra king":
     print(f"\nMy creator, {userName}. Pleasure to serve you!")
 else:
     print(f"\nHello, {userName}. Nice to meet you!")
-    size=input("\nWhat size pizza would you like? Enter small, medium, or large:  ")
-    flavor=input("\nWhat flavor of pizza?:  ")
-    flavor=input("Please enter a flavor: ")
-    crustType=input("\nWhat type of crust do you want:  ")
-quantity=input("n\How many pizza(s) do you want to order? Enter numeric value:  ")
-while not quantity. isdigit():
- quantity=int(quantity)
- method=input("\nIs this pickup or delivery?:  ")
- while method not in ["pickup","delivery"]:
-       method = input("please enter pickup or delivery: ")
 
- if .lower() == "delivery":
+    size=input("What size pizza would you like? Enter small, medium, or large:  ")
+    flavor=input("What flavor of pizza?:  ")
+    crustType=input("What type of crust do you want:  ")
+    quantity=input("How many pizza(s) do you want to order? Enter numeric value:  ")
+    method=input("Is this pickup or delivery:  ").lower()
+if:  
    deliveryFee =5
 else:
    deliveryFee =0
-salesTax=1.1
-
 if size.lower() == "small":
    pizzaCost= 8.99
 elif size.lower() == "medium":
@@ -30,7 +24,10 @@ elif size.lower() == "medium":
 elif size.lower() == "large":
     pizzaCost= 17.99
 
-total = (pizzaCost * quantity) * salesTax + deliveryFee
+salesTax=1.1
+
+total = (pizzaCost * quantity) * (1+salesTax) + deliveryFee
+print(f"Your total is: ${total:.2f}")
 print("-"*10)
 print(f"Thank you, {userName}, for your order.  ")
 print(f"your {quantity} {size} {flavor} pizza(s) with {crustType} crust costs $({total:,.2f}."  )
@@ -39,3 +36,4 @@ if total >= 50:
 else:
    print("\nOrder over $50 will receive $10 off coupon!  ")
    print("-"*10)
+
